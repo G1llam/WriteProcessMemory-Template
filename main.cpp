668fa118411c20fc6930dcdc77f33223a21bfb70
Gillam <41242486+G1llam@users.noreply.github.com>
2023-07-26 19:47:11 +0100

    Added pointer chaining
    added something called pointer chaining so you dont have to manually find the address where the money is stored each time you launch the game

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <vector>
 #include <Windows.h>
 
-DWORD pid;
-DWORD Credits = 0x133D2BE4; //Hex address for the games credits, as this isnt a pointer, you will need to find this each time - getting pointers is better
-int value{};
+int main() {
+    HWND GameWindow = FindWindowA(NULL, "Command & Conquer Red Alert 3: Uprising");
+    DWORD pID, gameBaseAddress, pointAddress, baseAddress, pointsAddress;
+        gameBaseAddress = 0x00400000;
+        pointAddress = 0x008FC734;
+        baseAddress = NULL;
+    GetWindowThreadProcessId(GameWindow, &pID);
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pID);
+    std::vector<DWORD> pointsOffsets{0x28, 0x40, 0x70, 0x04, 0x34, 0x00, 0x04};
 
-int main()
-{
-	HWND hwnd = FindWindowA(0, ("Command & Conquer Red Alert 3")); //find the games process window.
 
-	GetWindowThreadProcessId(hwnd, &pid); //get the games process ID
-	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid); //Opens the games process with all access
+    ReadProcessMemory(processHandle, (LPVOID)(gameBaseAddress + pointAddress), &baseAddress, sizeof(baseAddress), NULL);
+    pointsAddress = baseAddress;
+    for (int i = 0; i < pointsOffsets.size() - 1; i++)
+    {
+        ReadProcessMemory(processHandle, (LPVOID)(pointsAddress + pointsOffsets.at(i)), &pointsAddress, sizeof(pointsAddress), NULL);
+    }
+    pointsAddress += pointsOffsets.at(pointsOffsets.size() - 1);
 
-	std::cout << "How many credits would you like?: "; //Asks how many credits you would like to add
-	std::cin >> value; //stores the given amount of credits into the value variable
-
-	WriteProcessMemory(pHandle, (LPVOID)Credits, &value, sizeof(value), 0); //writes the new value to the game
+    std::cout << "Press Numpad 0 to EXIT" << std::endl;
+    std::cout << "Press Numpad 1 to set Points" << std::endl;
+    while (true) {
+        Sleep(50);
+        if (GetAsyncKeyState(VK_NUMPAD0)) {
+            return 0;
+        }
+        if (GetAsyncKeyState(VK_NUMPAD1)) {
+            std::cout << "How many credits would you like?" << std::endl;
+            int newPoints {};
+            std::cin >> newPoints;
+            WriteProcessMemory(processHandle, (LPVOID)(pointsAddress), &newPoints, sizeof(newPoints), NULL);
+        }
+    }
+    CloseHandle(processHandle);
+    return 0;
 }
